8abe686f9b2e009f494eabadacadeb038fee7b1a
Andrew Logan <andrewlogan@gmail.com>
2014-01-25 20:28:04 -0500

    Scrolls and makes noises in a second thread.

--- show_notes.py
+++ show_notes.py
@@ -8,55 +8,145 @@
 
 import sys
 import math
+import copy
 import pygame
+import pyaudio
 import argparse
-import random
+
+import struct
+import data_provider
 import musicxml_parse_test
+
+import threading
+
+#Shared between threads
+hertzen = None
+hertzen_lock = None
 
 class SpriteContainer:
     sprite = None
     pos = None
+    note = None
 
     #Sprite is a pygame.Surface object, pos is a tuple containing the x,y coordinates of where the sprite is on screen.
 
-    def __init__(self, sprite, pos):
+    def __init__(self, sprite, pos, note):
         self.sprite = sprite
         self.pos = pos
+        self.note = note
     
     #Moves the sprite some number of pixels from where it is
     def move(self, velocity):
         self.pos[0] += velocity[0]
         self.pos[1] += velocity[1]
 
-def make_note_sprites_from_notearray(notearray):
-    notes = []
-    for note_data in notearray:
-        when = note_data[0]
-        note = note_data[1]
-        duration_in_ms = note_data[2]
+def play_tones():
+    global hertzen, hertzen_lock
 
-        key = ""
+    print "Opening audio device..."
+
+    #44.1KHz is actually kind of pushing it, so do half that.
+    sample_rate = 22050
+
+    #Open the audio interface
+    p = pyaudio.PyAudio()
+
+    audio_stream = p.open(format=pyaudio.paFloat32,
+                          channels=1,
+                          rate=sample_rate,
+                          output=True)
+
+    previous_hertzen = None
+    current_hertzen = None
+
+    with hertzen_lock:
+        current_hertzen = copy.deepcopy(hertzen)
+
+    chunksize = 1
+
+    osc = data_provider.multi_oscillator(current_hertzen, sample_rate_in_hz=sample_rate, volume = .1, chunksize=chunksize)
         
-        #format is: [piano_key_#s], duration_in_seconds]
-        
-        if(note.step is None):
-            key = "R"
+    while 1:
+        previous_hertzen = current_hertzen
+        with hertzen_lock:
+            current_hertzen = copy.deepcopy(hertzen)
 
-        else:
-            key = note_to_key(note)
-            #print key
-            
-        #Update the previous entry if this note is in a chord
-        if(note.is_chord_member):
-            chord_notes = notes[-1][0]
-            chord_notes.append(key)
+        #Only update the oscillator if the frequencies have changed.
+        if(current_hertzen != previous_hertzen):
+            osc = data_provider.multi_oscillator(current_hertzen, sample_rate_in_hz=sample_rate, volume = .1, chunksize=chunksize)
 
-        else:
-            duration_in_sec = (duration_in_ms / 1000)
-            notes.append([[key], duration_in_sec])
+        previous_hertzen = current_hertzen
 
-    return notes
+        data = osc.get_data()
+        data_struct = struct.pack('f'*len(data), *data)
+        data_buffer = buffer(data_struct)
+        audio_stream.write(data_buffer)
 
+def key_to_freq(key):
+    # Derived from https://en.wikipedia.org/wiki/Piano_key_frequencies
+    #
+    # Key 1 is A0, key 88 is C8
+    #print key
+
+    num_harmonics = 0
+
+    twelfth_root_of_two = math.pow(2, float(1)/12)
+    freq = (math.pow(twelfth_root_of_two, (key - 49)) * 440)
+
+    freq_array = []
+
+    #Fundamental tone
+    freq_array.append(freq)
+
+    #Odd harmonics
+    for x in range(1, (num_harmonics * 2) + 1, 2):
+        freq_array.append(freq * x)
+
+    #Even harmonics
+    #for x in range(2, (num_harmonics * 2) +1, 2):
+    #    freq_array.append(freq * x)
+
+    #Plucked strings have all of the harmonics, but the odds are louder than the evens.  Let's simulate that by calculating the odd harmonics twice, I guess
+    for x in range(1, (num_harmonics + 1)):
+        freq_array.append(freq * x)
+
+    return sorted(freq_array)
+
+def note_to_key(note):
+    step = note.step
+    alter = note.alter  #sharps are positive
+    octave = note.octave
+
+    #A0 is 1, C1 is 4
+
+    #A is the first note in a 12-tone scale, D is the 6th, etc.
+    step_to_num = {'A' : 1,
+                   'B' : 3,
+                   'C' : 4,
+                   'D' : 6,
+                   'E' : 8,
+                   'F' : 9,
+                   'G' : 11}
+
+
+    #And handle sharps or flats
+    step_num = step_to_num[step]
+
+    if(alter is not None):
+        step_num + alter
+
+    #octave -= 1
+
+    #Octaves start at C
+    if(step_num > 3):
+        octave -= 1
+
+    #And calculate the key number.
+    key_num = step_num  + (octave * 12)
+
+    #print "{0}{1} is {2}".format(note.step, note.octave, key_num)
+
+    return key_num
 
 def positive_int(string):
     num = int(string)
@@ -66,6 +156,7 @@ def positive_int(string):
     return num
 
 def main(argv=None):
+    global hertzen, hertzen_lock
 
     #Get commandline arguments
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -83,10 +174,13 @@ def main(argv=None):
     windowHeight = args.wy
 
     fps = args.fps
+    
+    notearray = []
 
     if(args.infile):
         print "Loading..."
         music_xml = musicxml_parse_test.mxl_container(args.infile)
+
         notearray = music_xml.get_note_array()
 
     #Set up pygame
@@ -101,7 +195,7 @@ def main(argv=None):
     fontobj = pygame.font.Font(None, 50)
 
     #How quickly the sprites will scroll
-    ms_per_pixel = 10
+    ms_per_pixel = 1
 
     pixels_per_ms = 1.0 / ms_per_pixel
 
@@ -114,7 +208,7 @@ def main(argv=None):
     sprite_height = (windowHeight / num_strings)
 
     #Start near the right side of the screen
-    x_offset = math.ceil(windowWidth * .9)
+    x_offset = math.ceil(windowWidth * .3)
 
     for note_data in notearray:
         when = note_data[0]
@@ -147,7 +241,7 @@ def main(argv=None):
 
         start_y_pos = sprite_height * (guitar_string - 1)
 
-        sprite_container = SpriteContainer(sprite, [start_x_pos, start_y_pos])
+        sprite_container = SpriteContainer(sprite, [start_x_pos, start_y_pos], note)
 
         note_sprites.append(sprite_container)
 
@@ -159,12 +253,38 @@ def main(argv=None):
     ms_per_frame = 1000.0 / fps
     pixels_per_frame = pixels_per_ms * ms_per_frame
 
+    #samples / frame = ((samples / second) / 1000) * (ms / frame)
+
+    line_pos_x = windowWidth * .2
+
+    #Initialize the array
+    hertzen = []
+
+    #Initialize the lock object
+    hertzen_lock = threading.Lock()
+
+    #And give it to the audio thread.
+    audio_thread = threading.Thread(target=play_tones)
+
+    #Make the thread quit when the program quits
+    audio_thread.daemon = True
+
+    #launch thread!
+    audio_thread.start()
+
+    previous_hertzen = None
+    current_hertzen = []
+
     while 1:
         for event in pygame.event.get(): #check if we need to exit
                 if event.type == pygame.QUIT:pygame.quit();sys.exit()
 
-        screen.fill((100,100,100))
+        previous_hertzen = current_hertzen
 
+        current_hertzen = []
+
+        screen.fill((100,100,100))
+        
         for note_sprite in note_sprites:
             xpos = note_sprite.pos[0]
             xwidth = note_sprite.sprite.get_width()
@@ -174,13 +294,24 @@ def main(argv=None):
                (xpos <= windowWidth)):
                 screen.blit(note_sprite.sprite, note_sprite.pos)
 
-            #Scroll to the right
+                #Play notes that are crossing the line
+                if((line_pos_x >= xpos) and
+                   (line_pos_x <= xpos + xwidth)):
+
+                    current_hertzen.extend(key_to_freq(note_to_key(note_sprite.note)))
+
+
+            #Scroll sprite to the right
             note_sprite.move([-pixels_per_frame, 0])
 
             #print "{0} {1} {2} {3}".format(xpos, ypos, dx_per_frame, dy_per_frame)
 
+        if(previous_hertzen != current_hertzen):
+            with hertzen_lock:
+                hertzen = copy.deepcopy(current_hertzen)
+
         #Line should go over the notes
-        pygame.draw.line(screen, [255,255,255], [windowWidth * .2, 0], [windowWidth * .2, windowHeight], 5)
+        pygame.draw.line(screen, [255,255,255], [line_pos_x, 0], [line_pos_x, windowHeight], 5)
 
         pygame.display.flip() #RENDER WINDOW
         clk.tick(fps) #limit the fps
